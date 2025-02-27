b98a7251a6d237e1f2efa221dfc742e6dc281f70
e71828 <e71828@protonmail.ch>
2025-02-27 20:24:12 +0800

    version and readonly

--- advanced/Burn&Login.py
+++ advanced/Burn&Login.py
@@ -10,13 +10,32 @@ from stat import S_IREAD
 if __name__ == '__main__':
     proj = projects.primary
     if proj:
+        info = proj.get_project_info()
+        if not info.author:
+            info.author = "e71828"
+        current_version = info.version  # 类似于 .NET 中的 System.Version，其属性通常不可直接修改，
+        if current_version:
+            new_version = (
+                current_version.Major,
+                current_version.Minor,
+                current_version.Build + 1,
+                current_version.Revision
+            )
+        else:
+            new_version = (0, 1, 0, 0)
+        info.version = new_version  # 更新
         proj.save()
         path, name = os.path.split(proj.path)
         if 'Burned-on-' not in name:
             time_str = time.strftime("%Y%m%d-%H%M%S")
             filename = os.path.join(path, 'Burned-on-' + time_str + '.project')
             proj.save_as(filename, password='')
-            os.chmod(filename, S_IREAD)
+            # os.chmod(filename, S_IREAD)  # Can not sync to Onedrive
+            if not info.description:
+                info.description = 'Only for recording diff between all versions'
+            # And set the library to released
+            info.released = True
+            proj.save()
 
         # retrieve active application
         app = proj.active_application
